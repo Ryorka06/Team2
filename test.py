a9b3828ff50790d545edbbd2249e4cae0e57dc89
whwhwh25 <jojungmin1289@gmail.com>
2022-10-26 20:36:18 +0900

    2

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 def cal_score(score):
-
+    if score > 90:
+        print ('a')
+    elif 80 <= score <90:
+        print ('b')
+    elif 70 <= score <80:
+        print ('c')
+    elif 60 <= score <70:
+        print ('d')
+    else:
+        print ('f')
 def main():
+    score = int(input())
+    cal_score(score)
+
 
 if __name__ == "__main__":
     main()
